fc8148825df4169c90d33c800febe67f76d4b585
MatheusBueno <theusmagal@hotmail.com>
2024-11-11 12:30:30 +0200

    Simple Calculator complete

---
+++ examples/simple_calculator/simple_calculator.h
@@ -0,0 +1,30 @@
+// simple_calculator.h
+
+#ifndef SIMPLE_CALCULATOR_H
+#define SIMPLE_CALCULATOR_H
+
+/**
+ * Adds two integers.
+ * 
+ * Parameters:
+ * - a: First integer.
+ * - b: Second integer.
+ * 
+ * Returns:
+ * - Sum of a and b.
+ */
+int add(int a, int b);
+
+/**
+ * Subtracts the second integer from the first.
+ * 
+ * Parameters:
+ * - a: First integer.
+ * - b: Second integer.
+ * 
+ * Returns:
+ * - Result of a - b.
+ */
+int subtract(int a, int b);
+
+#endif // SIMPLE_CALCULATOR_H
